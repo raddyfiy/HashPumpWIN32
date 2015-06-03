1d76a269d18319ea3cc9123901ea8cf240f7cc34
Tim D. Smith <git@tim-smith.us>
2015-06-02 23:30:28 -0700

    don't install headers with Python module

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup (name = 'hashpumpy',
        version = '1.2',
        author      = 'Zach Riggle (Python binding), Brian Wallace (HashPump), Yen Chi Hsuan (Python3 support)',
        description = 'Python bindings for HashPump',
-       data_files  = [('include', glob('*.h'))],
        ext_modules = [module1],
        license     = 'MIT',
        url         = 'https://github.com/bwall/HashPump')
